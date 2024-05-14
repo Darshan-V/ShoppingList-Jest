8237db53b2ba9c5d2f9c9c0495387b2473650ec6
Darshan-V <mr.darshanv@gmail.com>
2024-05-14 17:14:49 +0530

    init layout for the app

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
     },
   ]);
   return (
-    <div className="app">
+    <div className="grid h-screen w-screen">
       <RouterProvider router={router} />
     </div>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex ">
+    <div className="flex dark:bg-red-950 bg-slate-200 h-full text-white w-full ">
       <main>{children}</main>
     </div>
   );

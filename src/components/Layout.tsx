29954a206b19ef3052f60b77189dec010b0e35db
Darshan-V <mr.darshanv@gmail.com>
2024-05-20 20:10:38 +0530

    create a grid layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import LeftCurtain from "./LeftCurtain";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,8 +9,19 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex dark:bg-red-950 bg-slate-200 h-full text-white w-full ">
-      <main>{children}</main>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 grid-rows-5 gap-0 h-screen">
+      <div className="bg-red-200 col-span-full sm:col-span-2 lg:col-span-5 row-span-1 flex justify-center items-center">
+        <Header />
+      </div>
+      <div className="hidden lg:flex bg-blue-200 col-span-1 row-span-3  justify-center items-center">
+        <LeftCurtain />
+      </div>
+      <main className="bg-green-200 col-span-full sm:col-span-2 lg:col-span-4 row-span-3 flex justify-center items-center">
+        {children}
+      </main>
+      <div className="bg-pink-200 col-span-full sm:col-span-2 lg:col-span-5 row-span-1 flex justify-center items-center">
+        <Footer />
+      </div>
     </div>
   );
 };
---
+++ src/components/LeftCurtain.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftCurtain = () => {
+  return <div>LeftCurtain</div>;
+};
+
+export default LeftCurtain;

87e25b7856e35124d5f2918961e2bf4fdcee84e5
Darshan-V <mr.darshanv@gmail.com>
2024-05-22 22:32:37 +0530

    fix layout and add menu sheet with mui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,16 +10,17 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 grid-rows-5 gap-0 h-screen">
-      <div className="bg-red-200 col-span-full sm:col-span-2 lg:col-span-5 row-span-1 flex justify-center items-center">
-        <Header />
-      </div>
-      <div className="hidden lg:flex bg-blue-200 col-span-1 row-span-3  justify-center items-center">
+      <div className="hidden lg:flex bg-blue-200 col-span-1 row-span-5  justify-center items-center">
         <LeftCurtain />
       </div>
+      <div className="bg-red-200 col-span-full sm:col-span-2 lg:col-span-4 row-span-1 flex justify-center items-center">
+        <Header />
+      </div>
+
       <main className="bg-green-200 col-span-full sm:col-span-2 lg:col-span-4 row-span-3 flex justify-center items-center">
         {children}
       </main>
-      <div className="bg-pink-200 col-span-full sm:col-span-2 lg:col-span-5 row-span-1 flex justify-center items-center">
+      <div className="bg-pink-200 col-span-full sm:col-span-2 lg:col-span-4 row-span-1 flex justify-center items-center">
         <Footer />
       </div>
     </div>
